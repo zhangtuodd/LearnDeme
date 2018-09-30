7bc9c5346e0d98bbea7ea7e5edf867cde3696bf4
张拓 <zhangtuodd@163.com>
2018-09-30 14:23:10 +0800

    mvvm之databinding 事件绑定(静态方法)

---
+++ app/src/main/java/mvvm/StringUtils.java
@@ -0,0 +1,16 @@
+package mvvm;
+
+/**
+ * 这里为了测试类方法
+ *
+ * @author zhangtuo
+ * @date 2018/9/30
+ */
+
+public class StringUtils {
+
+    public static String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+}
