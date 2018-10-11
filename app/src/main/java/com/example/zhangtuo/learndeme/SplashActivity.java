066d63865c6608b69898c739531e6a23e20179c4
张拓 <zhangtuodd@163.com>
2018-10-11 18:17:44 +0800

    启动图添加

---
+++ app/src/main/java/com/example/zhangtuo/learndeme/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.zhangtuo.learndeme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+/**
+ * 描述信息
+ *
+ * @author zhangtuo
+ * @date 2018/10/11
+ */
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(SplashActivity.this, WebActivity.class));
+//                finish();
+//            }
+//        }, 2000);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
