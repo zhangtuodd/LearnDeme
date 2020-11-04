c7e3cbf3559876e4684de0981e0879b60b1b85a0
张拓 <zhangtuodd@163.com>
2020-11-04 19:54:48 +0800

    模拟内存抖动

--- acfun/src/main/java/client/cloudy/com/acfun/AcFunActivity.java
+++ acfun/src/main/java/client/cloudy/com/acfun/AcFunActivity.java
@@ -1,5 +1,8 @@
 package client.cloudy.com.acfun;
 
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,5 +19,18 @@ public class AcFunActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acfun);
+//        handler.sendEmptyMessageDelayed(0, 30);
     }
+
+//    @SuppressLint("HandlerLeak")
+//    private static Handler handler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            for (int i = 0; i < 100; i++) {
+//                String[] a = new String[100000];
+//            }
+//            handler.sendEmptyMessageDelayed(0, 30);
+//        }
+//    };
 }
